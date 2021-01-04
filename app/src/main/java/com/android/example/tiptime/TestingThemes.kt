a96a9c05a08d2a8fb331b627b72bdad670586d9b
Luis <luchopintado@gmail.com>
2021-01-04 16:04:55 -0500

    Change the app theme

---
+++ app/src/main/java/com/android/example/tiptime/TestingThemes.kt
@@ -0,0 +1,11 @@
+package com.android.example.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestingThemes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testing_themes)
+    }
+}
